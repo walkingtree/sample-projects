5305284deb5713c66ee66d8b78f76bc6728a7b42
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2017-07-28 16:52:47 +0100

    Fixed working with ExtJS 6.5 and Angular 4.x

--- angluar2/AngularExt/packages/local/AngularAppComponent/src/ngComponent.js
+++ angluar2/AngularExt/packages/local/AngularAppComponent/src/ngComponent.js
@@ -2,48 +2,39 @@ Ext.define("AngularAppComponent.ngComponent",{
    extend:'Ext.Component',
     alias:'widget.ngapp',
     config:{
-        ngRootTag:"app-root",
-        ngAppPath:"./angularapp",
+        angularRootTag:"app-root",
+        angularAppPath:"./angularapp",
     },
     afterRender:function() {
         var me = this;
         this.callParent(arguments);
         Ext.Ajax.request({
-            url:me.getNgAppPath()+"/index.html",
-            success:function(response){
+            url: me.getAngularAppPath() + "/index.html",
+            success: function(response) {
                 var el = document.createElement('html');
                 el.innerHTML = response.responseText;
                 var tags = [
                     "inline.bundle.js",
                     "polyfills.bundle.js",
                     "styles.bundle.js",
-                    //"vendor.bundle.js",
+                    //"vendor.bundle.js", TODO: investigate why is it commented?
                     "main.bundle.js"
                 ];
-                var newEl = document.createElement("base");
-                newEl.setAttribute("href",me.getNgAppPath()+"/");
-                me.el.appendChild(newEl);
-                for( var i=0;i<tags.length;i++){
-
-
-                        var newEl = document.createElement('script');
-                            newEl.setAttribute("type","text/javascript");
-                            newEl.setAttribute("src",tags[i]);
-                        me.el.appendChild(newEl);
-
+                
+                var angularAppPath = me.getAngularAppPath()+"/";
+                for(var i = 0; i < tags.length; i++){
+                    var scriptElement = document.createElement('script');
+                    scriptElement.setAttribute("type", "text/javascript");
+                    scriptElement.setAttribute("src", angularAppPath + tags[i]);
+                    
+                    me.el.appendChild(scriptElement);
                 }
 
-                 newEl = document.createElement(me.getNgRootTag());
-                me.el.appendChild(newEl);
-
-
+                var angularRootElement = document.createElement(me.getAngularRootTag());
+                me.el.appendChild(angularRootElement);
             },
-            failure:function(){
-
+            failure: function() {
             }
         });
     }
-
-
-
 });
